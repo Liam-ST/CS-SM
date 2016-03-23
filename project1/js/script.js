acd60aea07ee3d4033d038cfe57d750e0ea2024f
Liam S <liam.schmidt172@gmail.com>
2016-03-23 19:07:22 -0400

    jScript Plugins
    Added 2 plugins in the "players" section -- removed ui.js

--- project1/js/script.js
+++ project1/js/script.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
-    $('.menu-contain').click(function() {
+	$('.menu-contain').click(function() {
         $(".list").toggleClass("show");
     });
-});
+    var filterizd = $('.filtr-container').filterizr({
+   //options object
+	layout: 'packed',
+	animationDuration: .3,
+	});
+	$('#search').hideseek();
+	$('#search').on("_after_each", function() {
+		$('.player').css("background-color: red;")
+	});
 
+
+
+
+
+
+
+
+});
