00d73cbfad5f846386726ca843fbc1e99ca66791
Liam S <liam.schmidt172@gmail.com>
2016-03-25 03:02:42 -0400

    revised players page
    completely changed players section with a small script rather than
    plugin

--- project1/js/script.js
+++ project1/js/script.js
@@ -2,21 +2,31 @@ $(document).ready(function() {
 	$('.menu-contain').click(function() {
         $(".list").toggleClass("show");
     });
-    var filterizd = $('.filtr-container').filterizr({
-   //options object
-	layout: 'packed',
-	animationDuration: .3,
-	});
-	$('#search').hideseek();
-	$('#search').on("_after_each", function() {
-		$('.player').css("background-color: red;")
-	});
+    //START SEARCH SCRIPT//
+
+    $("#search").keyup(function () {
+    	var filter = $(this).val(), 
+        count = 0;
+
+    $(".player p:first-child").each(function () {
+        var parent = $(this).parent(), length = $(this).text().length>0;
+        if ( length && $(this).text().search(new RegExp(filter, "i")) < 0) {
+     	 parent.hide();
+         }
+        else {
+     	 parent.show();
+      	 count++;
+         }
+    });
+    });
 
 
+    function myFunction(x) {x.classList.toggle("change");}
 
-
-
-
-
-
+    $(function() {
+  	$('.fa-chevron-circle-down').click(function() {
+    $('.players-container').css('overflow', 'visible');
+    $('.fa-chevron-circle-down').css('display', 'none');
+  	});
+});
 });
